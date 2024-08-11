4b63555f776f3b67fa6c56ea5500f69d6b0dbfc1
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-08-12 00:37:00 +0300

    Start ¶The main game scene

---
+++ Mob.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+
+func _ready():
+	
+	# play the animation and randomly choose one of the three animation types:
+	$AnimatedSprite.playing = true
+	var mob_types = $AnimatedSprite.frames.get_animation_names()
+	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
