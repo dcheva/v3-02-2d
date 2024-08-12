af4d8453532615617ac02913af9199fba022c233
Dmytro Cheva <dmytro.cheva@gmail.com>
2024-08-12 14:33:35 +0300

    up

--- HUD.gd
+++ HUD.gd
@@ -11,7 +11,7 @@ func show_game_over():
 	show_message("Game Over")
 	# Wait until the MessageTimer has counted down.
 	yield($MessageTimer, "timeout")
-	$Message.text = "Мочи Крепов!!!"
+	$Message.text = "Тiкай iз мiста,\nбо тобi пiз"
 	$Message.show()
 	# Make a one-shot timer and wait for it to finish.
 	yield(get_tree().create_timer(1), "timeout")
